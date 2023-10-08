1969d52127c28adeb7b34d6e4446b4ad87dc1437
alex.shitov1237 <alex.shitov1237@gmail.com>
2023-10-08 09:11:35 +0200

    move main to other file

---
+++ lesson_4/function_main.c
@@ -0,0 +1,11 @@
+extern void print42();
+extern void print_str();
+extern int input();
+
+int main() {
+    print42();
+    print_str();
+    input();
+    return 0;
+}
+
--- lesson_4/main.c
+++ lesson_4/main.c
@@ -1,11 +1,18 @@
+#include <stdio.h>
+
 extern void print42();
 extern void print_str();
-extern int input();
+extern void input();
 
 int main() {
-    print42();
-    print_str();
-    input();
+    int check;
+    scanf("%d", &check);
+    void (*array[3])(void) = {print42, print_str, input};
+    if (check <= 0 || check > 3) {
+        return 1;
+    } else {
+        array[check - 1](); 
+    }
     return 0;
 }
 
